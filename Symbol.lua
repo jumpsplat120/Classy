088647837ec3890c1c6b7cc278a81ea8c195c31f
jumpsplat120 <jumpsplat120@yahoo.com>
2023-12-02 21:39:25 -0800

    Fix file_path issue
    Basically the file_path was different for Symbol versus any other class, so it redefined private and then couldn't see the private that Object was using

--- Symbol.lua
+++ Symbol.lua
@@ -1,15 +1,16 @@
-local Object, private, file_path, Symbol
+local Object, Symbol, private, file_path
 
-file_path = (...):match("(.-)[^%.]+$")
-private   = require(file_path .. "instances")
-Object    = require(file_path)
+--".Symbol" is 7 characters, so we -8 to get just
+--the directory.
+file_path = (...):sub(1, -8)
+
+private = require(file_path .. ".instances")
+Object  = require(file_path)
 
 Symbol = Object:extend()
 
 function Symbol:new(id)
-    private[self] = {
-        id = id and tostring(id) or math.uuid()
-    }
+    private[self].id = id and tostring(id) or math.uuid()
 end
 
 function Symbol.__get:id() 
